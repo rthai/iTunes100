70a596d6178ba8ce82ddd3f2c2e1e9ed06b22f91
rthai <ahnamiko@gmail.com>
2018-10-11 19:39:33 -0700

    fetch from itunes feed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import List from './List';
+import Subnav from './Subnav';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      albums: [],
+    }
+  }
+
+  componentDidMount() {
+    const endpoint = `https://itunes.apple.com/us/rss/topalbums/limit=100/json`;
+    
+    fetch(endpoint)
+      .then(blob => blob.json())
+      .then(data => {
+        let albums = data.feed.entry.map(album => {
+          let copy = {};
+          for (let key in album) {
+            if (key.match(/im:/)) {
+              let newKey = key.replace(/im:/, '');
+              copy[newKey] = album[key];
+            } else {
+              copy[key] = album[key];
+            }
+          }
+          return copy;
+        }); 
+        
+        this.setState({ albums });
+      })
+      .catch(err => console.error(err));
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Subnav/>
+        <List albums={this.state.albums}/>
       </div>
     );
   }
